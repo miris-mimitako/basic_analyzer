0914a00e0b78b29e221b62c856cddacf30f4e5e5
miris-mimitako <miris.mikitako@gmail.com>
2022-09-24 17:07:01 +0900

    add: histgram

--- basicanalyzer.py
+++ basicanalyzer.py
@@ -12,7 +12,8 @@ import datetime
 import markdown
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-
+from see import see
+import scipy.stats as stats
 
 class BasicAnalyze:
   """
@@ -75,6 +76,25 @@ csv file: {csv_path}
         # Make Graph
         dfT = self.df_record.T
         GM = GraphMaker(self.create_folder_path, dfT, df, csv_path)
+
+        # Make graph 1 each
+        list_graph_data = GM.hist_plot()
+
+        for content in list_graph_data:
+          link = content = '.'+ content[content.rfind('\\'):]
+
+          text6 = (
+f"""
+#### Histgram and Q-Q plot
+
+Histgram and Q-Q plot
+
+[![]({link})]({link})
+
+"""          
+          )
+          self.write_record(text6)
+
         heat_map_path = GM.heat_map()
         heat_map_path = '.'+ heat_map_path[heat_map_path.rfind('\\'):]
         text2 = (
@@ -103,6 +123,14 @@ Scatter plot
 """          
           )
           self.write_record(text3)
+        ##E for
+
+        SR = StatisticRecord(self.create_folder_path, dfT, df, csv_path)
+        text_result = SR.ols_record()
+
+        for text_data in text_result:
+          text4 = (f"```\n\n{text_data}\n\n```\n\n")
+          self.write_record(text4)
 
         self.encoding_mark_down_to_html()
 
@@ -229,12 +257,25 @@ Scatter plot
     pass
 
 class StatisticRecord:
-  def __init__(self) -> None:
-    pass
+  def __init__(self, location, dataframe, df, csv_path) -> None:
+    self.save_file_location = location
+    self.structure_data = dataframe # dataframe, 
+    self.df = df
+    self.csv_name = csv_path[csv_path.rfind("\\")+1:csv_path.rfind(".")]
 
   def ols_record(self):
-    pass
-
+    _df = self.df
+    list_record = []
+    _df.dropna(inplace=True)
+    for xindex, x_column in enumerate(_df.columns):
+      for yindex, y_column in enumerate(_df.columns):
+        if xindex < yindex:
+          if ("int" or "float") in str(_df[x_column].dtype) and ("int" or "float") in str(_df[y_column].dtype):
+            model = sm.OLS(_df[y_column] ,_df[x_column])
+            res = model.fit()
+            _text = x_column + "_vs_" + y_column + "\n\n" + str(res.summary()) 
+            list_record.append(_text)
+    return list_record
   def __del__(self):
     pass
 
@@ -274,6 +315,24 @@ class GraphMaker:
       ##E for
     ##E for
     return list_save_location
+
+  def hist_plot(self):
+    list_save_location = []
+
+    list_index = self.structure_data[self.structure_data.Data_Type.str.contains('Int') | self.structure_data.Data_Type.str.contains('Float') | self.structure_data.Data_Type.str.contains('Date')].index
+    for column in list_index:
+      fig, (ax1, ax2) = plt.subplots(1,2, figsize = (20, 10))
+      sns.histplot(data = self.df, x = column, kde = True, ax = ax1)
+      sm.qqplot(self.df[column].values, ax=ax2)
+      
+      file_path = os.path.join(self.save_file_location, self.csv_name + "-" + column + "-" + "Q-Q_plot.png")
+      ax1.set_title("Histgram of " + column)
+      ax2.set_title("Q-Q plot of " + column)
+      fig.savefig(file_path)
+      plt.close()
+      list_save_location.append(file_path)
+    return list_save_location
+
   def __del__(self):
     pass
 
